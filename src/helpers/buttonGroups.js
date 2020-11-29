6ad33b3e834edd8afb4bbea5f7bd0687a9548f22
Bluette1 <marylene.sawyer@gmail.com>
2020-11-29 15:28:48 +0200

    Fix eslint errors

--- src/helpers/buttonGroups.js
+++ src/helpers/buttonGroups.js
@@ -2,7 +2,7 @@ const buttons = [
   { itemList0: ['AC', '+/-', '%', '÷'] },
   { itemList1: ['7', '8', '9', 'x'] },
   { itemList2: ['4', '5', '6', '-'] },
-  {itemList3: ['1', '2', '3', '+']},
+  { itemList3: ['1', '2', '3', '+'] },
   { itemList4: ['0', '.', '='] },
 ];
 const buttonGroups = () => buttons;
