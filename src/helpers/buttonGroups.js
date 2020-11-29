8510170de9d123515c8b70907762e64c37ecaaa2
Bluette1 <marylene.sawyer@gmail.com>
2020-11-29 15:22:44 +0200

    Fix button panel

--- src/helpers/buttonGroups.js
+++ src/helpers/buttonGroups.js
@@ -2,7 +2,8 @@ const buttons = [
   { itemList0: ['AC', '+/-', '%', '÷'] },
   { itemList1: ['7', '8', '9', 'x'] },
   { itemList2: ['4', '5', '6', '-'] },
-  { itemList3: ['0', '.', '='] },
+  {itemList3: ['1', '2', '3', '+']},
+  { itemList4: ['0', '.', '='] },
 ];
 const buttonGroups = () => buttons;
 export default buttonGroups;
