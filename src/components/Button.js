3beac82baa674753d66f50ed83c572381fe0907a
Bluette1 <marylene.sawyer@gmail.com>
2020-12-09 16:13:31 +0200

    Add styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../Button.css';
 
-const Button = ({ value, onclick }) => <button type="button" onClick={() => onclick(value)}>{value}</button>;
+const Button = ({
+  value, onclick, wide, color,
+}) => (
+  <button
+    type="button"
+    style={{
+      width: value === '0' ? '50%' : wide,
+      backgroundColor: color,
+    }}
+    onClick={() => onclick(value)}
+  >
+    {value}
+  </button>
+);
 
 Button.propTypes = { value: PropTypes.string.isRequired };
 Button.propTypes = { onclick: PropTypes.func.isRequired };
+Button.propTypes = { wide: PropTypes.func.isRequired };
+Button.propTypes = { color: PropTypes.string };
+
+Button.defaultProps = {
+  color: '#ff7b00',
+};
 
 export default Button;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './App.css';
 import App from './components/App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App className="App" />
   </React.StrictMode>,
   document.getElementById('root'),
 );
