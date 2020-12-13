1ebde801444a947db995c6a76a3af03415ce2d12
Bluette1 <marylene.sawyer@gmail.com>
2020-12-13 11:53:59 +0200

    Add tests for `+/-` and `%`

--- test/calculate.test.js
+++ test/calculate.test.js
@@ -8,3 +8,30 @@ test('calculate - when buttonName equals `+/-` and total is set', () => {
   }
   expect(calculate(calcObj, '+/-')).toEqual(-4);
 });
+
+test('calculate - when buttonName equals `+/-` and next is set', () => {
+  const calcObj = {
+    total: null,
+    next: 4,
+    operation: null,
+  }
+  expect(calculate(calcObj, '+/-')).toEqual(-4);
+});
+
+test('calculate - when buttonName equals `%` and total is set', () => {
+  const calcObj = {
+    total: 4,
+    next: null,
+    operation: null,
+  }
+  expect(JSON.stringify(calculate(calcObj, '%'))).toEqual(JSON.stringify('0.04'));
+});
+
+test('calculate - when buttonName equals `%` and next is set', () => {
+  const calcObj = {
+    total: null,
+    next: 4,
+    operation: null,
+  }
+  expect(JSON.stringify(calculate(calcObj, '%'))).toEqual(JSON.stringify('0.04'));
+});
