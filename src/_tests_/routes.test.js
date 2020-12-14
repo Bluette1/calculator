c4e5326644e065c8208fa5add5c6e2b542555982
Bluette1 <marylene.sawyer@gmail.com>
2020-12-14 08:17:47 +0200

    Add tests for routes

---
+++ src/_tests_/routes.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import Routes from '../Routes';
+import App from '../components/App';
+import Quote from '../layouts/Quote';
+import Home from '../layouts/Home';
+import NotFound from '../layouts/NotFound';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { MemoryRouter
+} from 'react-router'
+import { Route } from 'react-router-dom';
+
+configure({ adapter: new Adapter() });
+
+let pathMap = {};
+describe('routes using array of routers', () => {
+  beforeAll(() => {
+    const component = shallow(<Routes/>);
+    pathMap = component.find(Route).reduce((pathMap, route) => {
+        const routeProps = route.props();
+        pathMap[routeProps.path] = routeProps.component;
+        return pathMap;
+      }, {});
+      console.log(pathMap)
+  })
+  it('should show Home component for / router (getting array of routes)', () => {
+
+    expect(pathMap['/']).toBe(Home);
+  })
+  it('should show App component for /news router', () => {
+    expect(pathMap['/calculator']).toBe(App);
+  })
+  it('should show Quote component techdomain for /news router', () => {
+    expect(pathMap['/quote']).toBe(Quote);
+  })
+  it('should show No  component for route not defined', ()=>{
+      expect(pathMap['undefined']).toBe(NotFound);
+  })
+})
